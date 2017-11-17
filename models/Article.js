bf565c2a9f0c5b1d7df2f0dad973d01f9ff119ac
Joseph Kohatsu <jpkohatsu@gmail.com>
2017-11-17 10:29:18 -0600

    models js edits

--- models/Article.js
+++ models/Article.js
@@ -5,25 +5,25 @@ var Schema = mongoose.Schema;
 
 // Create article schema
 var ArticleSchema = new Schema({
-  // title is a required string
-  title: {
-    type: String,
-    required: true
-  },
-  // link is a required string
-  link: {
-    type: String,
-    required: true
-  },
-  // This only saves one note's ObjectId, ref refers to the Note model
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  },
-  saved:{
-    type: Boolean,
-    required: true
-  }
+    // title is a required string
+    title: {
+        type: String,
+        required: true
+    },
+    // link is a required string
+    link: {
+        type: String,
+        required: true
+    },
+    // This only saves one note's ObjectId, ref refers to the Note model
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    },
+    saved: {
+        type: Boolean,
+        required: true
+    }
 });
 
 // Create the Article model with the ArticleSchema
